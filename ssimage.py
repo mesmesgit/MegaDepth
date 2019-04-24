a0e92ac19cdc62970755a8335495bd2d703783fd
Mark Strickland <stricklandmes@gmail.com>
2019-04-24 14:03:43 -0700

    bug fix ssimage

--- ssimage.py
+++ ssimage.py
@@ -9,19 +9,21 @@ mpl.use('Agg')
 #
 import matplotlib.pyplot as plt
 import sys
-sys.path.insert(0, '../pspnet-pytorch')
+sys.path.insert(0, '../pspnet-pytorch/')
 import semseg
 #
 # set the input image filename
 # set the output image filenames
-config = '../pspnet-pytorch/config/ade20k.yaml'
-image_path = '../MegaDepth/docs/vertical-street.jpg'
+platform_rootpath = '/home/dockeruser/Documents/AVCES/'
+pspnetpath = os.path.join(platform_rootpath, 'pspnet-pytorch/')
+config = os.path.join(pspnetpath, 'config/ade20k.yaml')
+image_path = os.path.join(platform_rootpath, 'MegaDepth/docs/vertical-street.jpg')
 cuda = True
 crf = True
 # out_class_figure = 'docs/demo_out.png'
 out_masked_sky_image = 'docs/image-sky-masked.png'
 # run semantic segmentation and get the masked image
-masked_image = semseg.semseg(config, image_path, cuda, crf)
+masked_image = semseg.semseg(pspnetpath, config, image_path, cuda, crf)
 # save the masked image
 plt.imsave(out_masked_sky_image, masked_image)
 #

a50b2e4a66b19d70fd6a6a8dfb735afd37d2b4eb
Mark Strickland <stricklandmes@gmail.com>
2019-04-24 16:13:14 -0700

    rev to ss-depth

--- ss-depth.py
+++ ss-depth.py
@@ -53,8 +53,8 @@ plt.imsave(out_masked_sky_image, masked_image)
 #
 model = create_model(opt)
 
-input_height = 384
-input_width  = 512
+input_height = 256
+input_width  = 256
 
 total_loss =0
 toal_count = 0

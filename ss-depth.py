ac582ac2f41a6655a762c8287786b902bd15e923
Mark Strickland <stricklandmes@gmail.com>
2019-04-24 16:32:34 -0700

    bug fix ss-depth

--- ss-depth.py
+++ ss-depth.py
@@ -79,7 +79,8 @@ pred_depth = torch.exp(pred_log_depth)
 
 # visualize prediction using inverse depth, so that we don't need sky segmentation (if you want to use RGB map for visualization, \
 # you have to run semantic segmentation to mask the sky first since the depth of sky is random from CNN)
-pred_inv_depth = 1.0/pred_depth
+# pred_inv_depth = 1.0/pred_depth
+pred_inv_depth = pred_depth
 pred_inv_depth = pred_inv_depth.data.cpu().numpy()
 # you might also use percentile for better visualization
 pred_inv_depth = pred_inv_depth/np.amax(pred_inv_depth)
